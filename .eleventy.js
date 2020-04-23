5a0d30f8ac48ccc1ee5eade086ab47e3c72f3689
Mark Hougaard Jensen <mark@marks.dk>
2020-04-23 13:05:04 +0200

    fix(url): Prefixed with repo name

--- .eleventy.js
+++ .eleventy.js
@@ -6,4 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('cssmin', function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
+  return {
+    pathPrefix: '/mapsindoors.com/',
+  };
 };
