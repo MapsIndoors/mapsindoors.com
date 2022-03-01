2c9facae1996345699affab578dfbb6a871a8327
Mark Hougaard <mark@marks.dk>
2022-03-01 11:25:30 +0100

    Updates

--- .eleventy.js
+++
@@ -1,9 +0,0 @@
-const CleanCSS = require('clean-css');
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('img');
-  eleventyConfig.addPassthroughCopy('favicon.ico');
-
-  eleventyConfig.addFilter('cssmin', function (code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
-};
